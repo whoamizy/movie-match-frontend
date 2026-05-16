fb90124f0ab00b220c615eb6f73048bd9b7b9a30
whoamizy <whoamizy@yandex.ru>
2026-05-16 05:14:35 +0400

    first page

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <main class="body page">
+    <section class="container py-12 flex min-h-screen items-center">
+      <div class="surface w-full">
+        <h1 class="text-5xl heading text-center md:text-8xl sm:text-7xl">
+          Movie Match
+        </h1>
+      </div>
+    </section>
+  </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2026-05-14',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@unocss/nuxt'],
-  css: ['@unocss/reset/tailwind.css'],
+  css: ['@unocss/reset/tailwind.css', '~/assets/styles/theme.css'],
 })
