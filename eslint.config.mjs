b35ad9708d39afb847c36d23dcfa9435c6e9dc85
whoamizy <whoamizy@yandex.ru>
2026-05-16 05:36:47 +0400

    remove mention .agents/skills

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,6 @@ import prettierPlugin from 'eslint-plugin-prettier'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  {
-    ignores: ['.agents/**'],
-  },
   unocss,
   {
     plugins: {
