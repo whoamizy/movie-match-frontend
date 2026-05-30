da1ce981c96f887564bd87764b3f63743f755a7e
whoamizy <whoamizy@yandex.ru>
2026-05-30 15:34:09 +0400

    fix: ui button

--- app/components/ui/Button.vue
+++ app/components/ui/Button.vue
@@ -1,10 +1,13 @@
 <template>
-  <button :type="type" class="button" :disabled="disabled">
+  <button :type="type" :class="buttonClass" :disabled="disabled">
     <slot />
   </button>
 </template>
 
 <script setup lang="ts">
+const buttonClass =
+  'inline-flex items-center justify-center rounded-md bg-button px-4 py-2 text-sm text-button-foreground font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'
+
 withDefaults(
   defineProps<{
     disabled?: boolean
